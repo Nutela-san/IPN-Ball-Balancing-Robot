1c62baed24b541b2228de7d07149789a127a253d
RoyPineda <99355624+RoyPineda@users.noreply.github.com>
2023-07-13 19:32:37 -0600

    Update MicroCode y python
    Se modifico la librería de servo para tener funciones para complementar el movimiento, para definir rangos de pwm y de movimiento. También se probo el movimiento conjunto de los 3 servos ya con los valores calibrados

--- Balancin_IPN_ESP32_code/include/ServoController.h
+++ Balancin_IPN_ESP32_code/include/ServoController.h
@@ -11,11 +11,16 @@ float map_f(float valor, float min_rango_original, float max_rango_original ,flo
 class ServoControl{
 public:
   uint8_t id_pwm_chanel, pwm_bit_resolution, pwm_freq, pwm_pin;
-  unsigned int pwm_range[2]; // [0]:min, [1]:max
-  unsigned int max_pwm_value = pow(2,pwm_bit_resolution)-1;
-  float periodo = 1000.0/((float)pwm_freq); 
+  float pwm_range[2]; // [0]:min, [1]:max
+  unsigned int max_pwm_value;
+  float periodo; 
+
+  uint8_t move_range[2];     // [0]:min, [1]:max
+  uint8_t curret_pos;
+  bool complementar = false; // variable para activar el complemento de los angulos
+  uint8_t init_pos;
   
-  ServoControl(uint8_t PWMchanel , uint8_t PWM_pin, uint8_t PWM_bitResotution=16, uint8_t PWM_Frequency = 50)
+  ServoControl(uint8_t PWMchanel , uint8_t PWM_pin, uint8_t PWM_bitResotution=12, uint8_t PWM_Frequency = 50)
   {
     this->pwm_pin = PWM_pin;
     id_pwm_chanel = PWMchanel;
@@ -24,24 +29,49 @@ public:
   }
 
   void begin(){
+    max_pwm_value = pow(2,pwm_bit_resolution)-1;
     periodo = 1000.0/((float)pwm_freq);
-    pwm_range[0] = (int)map_f(1,0,periodo,0,max_pwm_value);
-    pwm_range[1] = (int)map_f(2,0,periodo,0,max_pwm_value);
+    pwm_range[0] = map_f(1.0,0.0,periodo,0.0,max_pwm_value);
+    pwm_range[1] = map_f(2.0,0.0,periodo,0.0,max_pwm_value);
 
     pinMode(pwm_pin,OUTPUT);
     ledcSetup(id_pwm_chanel,pwm_freq,pwm_bit_resolution);
     ledcAttachPin(pwm_pin,id_pwm_chanel);
-    this->set_pos(90);
+    this->set_pos(0);
+  }
+
+  void begin(unsigned int pwm_valor_min, unsigned int pwm_valor_max, uint8_t rango_movimiento_min, uint8_t rango_movimiento_max, uint8_t pos_inicial, bool invertir){
+
+    pwm_range[0] = pwm_valor_min;
+    pwm_range[1] = pwm_valor_max;
+    move_range[0] = rango_movimiento_min;
+    move_range[1] = rango_movimiento_max;
+    curret_pos = pos_inicial;
+    init_pos = pos_inicial;
+    complementar = invertir;
+
+    pinMode(pwm_pin,OUTPUT);
+    ledcSetup(id_pwm_chanel,pwm_freq,pwm_bit_resolution);
+    ledcAttachPin(pwm_pin,id_pwm_chanel);
+    this->set_pos(0);
   }
 
   uint16_t degrees2pwm(uint8_t degrees)
   {
+    if (complementar)
+    {
+      degrees = (uint8_t)180 - degrees;
+    }
     return ((uint16_t)map(degrees, 0, 180, pwm_range[0], pwm_range[1]));
   }
 
   void set_pos(uint8_t degrees)
   {
-    ledcWrite(id_pwm_chanel,degrees2pwm(degrees));
+    if (degrees >= move_range[0] && degrees <= move_range[1])
+    {
+      ledcWrite(id_pwm_chanel,degrees2pwm(degrees));
+      curret_pos = degrees;
+    }
   }
 
   void print_info_servo()
@@ -53,6 +83,11 @@ public:
     Serial.print(pwm_range[0]);
     Serial.print(" - ");
     Serial.println(pwm_range[1]);
+  }
+
+  void initial_pos()
+  {
+    set_pos(init_pos);
   }
 };
 
--- Balancin_IPN_ESP32_code/src/main.cpp
+++ Balancin_IPN_ESP32_code/src/main.cpp
@@ -2,24 +2,51 @@
 #include <InterCom.h>
 #include <ServoController.h>
 
+uint8_t pines_servos[3]= {13,26,14};
+
 SimpleCommand cmd;
 
-ServoControl servo1(0,5);
+ServoControl servo1(0,pines_servos[0],14,50), servo2(1,pines_servos[1],14,50), servo3(2,pines_servos[2],14,50);
 
-float pos = 0, last_pos = 0;
+
+float pos = 20, last_pos = 30;
+bool test = false;
+
+unsigned long last_time = 0;
 
 void list(){
   cmd.list();
 }
 
+void popo(){
+  servo1.print_info_servo();
+}
+
+void doTest(){
+  last_time = millis();
+  test = !test;
+}
+
 void setup() {
-  servo1.begin();
+  servo1.begin(400,2000,0,120,30,true);
+  servo2.begin(450,2000,0,120,30,true);
+  servo3.begin(400,2000,0,120,30,true);
 
   cmd.enable_echo(true);
   cmd.addCommand("list",list);
+  cmd.addCommand("test",doTest);
+  cmd.addCommand("min",&servo1.pwm_range[0]);
+  cmd.addCommand("max",&servo1.pwm_range[1]);
   cmd.addCommand("p",&pos);
+  
 
   cmd.begin(115200);
+
+
+  servo1.initial_pos();
+  servo2.initial_pos();
+  servo3.initial_pos();
+
 }
 
 void loop() {
@@ -28,6 +55,18 @@ void loop() {
   if(pos != last_pos){
     last_pos = pos;
     servo1.set_pos((int)last_pos);
+    servo2.set_pos((int)last_pos);
+    servo3.set_pos((int)last_pos);
+  }
+
+  if(test && (millis()-last_time)>= 1500){
+    if(pos < 30){
+      pos = 80;
+    }
+    else if(pos > 70){
+      pos = 20;
+    }
+    last_time = millis();
   }
 }
 
