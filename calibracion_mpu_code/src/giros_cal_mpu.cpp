db5ead2ebc360ae6d3ac63c09f6c8ce1beec5f5c
RoyPineda <99355624+RoyPineda@users.noreply.github.com>
2023-09-28 15:19:52 -0600

    feate calibracion giroscopio
    calibramos utilizando el tutorial
    https://naylampmechatronics.com/blog/45_tutorial-mpu6050-acelerometro-y-giroscopio.html

---
+++ calibracion_mpu_code/src/giros_cal_mpu.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+// Librerias I2C para controlar el mpu6050
+// la libreria MPU6050.h necesita I2Cdev.h, I2Cdev.h necesita Wire.h
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+
+// La dirección del MPU6050 puede ser 0x68 o 0x69, dependiendo 
+// del estado de AD0. Si no se especifica, 0x68 estará implicito
+MPU6050 sensor;
+
+// Valores RAW (sin procesar) del acelerometro y giroscopio en los ejes x,y,z
+int ax, ay, az;
+int gx, gy, gz;
+
+long tiempo_prev;
+float dt;
+float ang_x, ang_y;
+float ang_x_prev, ang_y_prev;
+
+void setup() {
+  Serial.begin(115200);    //Iniciando puerto serial
+  Wire.begin();           //Iniciando I2C  
+  sensor.initialize();    //Iniciando el sensor
+
+  if (sensor.testConnection()) Serial.println("Sensor iniciado correctamente");
+  else Serial.println("Error al iniciar el sensor");
+}
+
+void loop() {
+  // Leer las aceleraciones y velocidades angulares
+  sensor.getAcceleration(&ax, &ay, &az);
+  sensor.getRotation(&gx, &gy, &gz);
+  float ax_m_s2 = ax * (9.81/16384.0);
+  float ay_m_s2 = ay * (9.81/16384.0);
+  float az_m_s2 = az * (9.81/16384.0);
+  float gx_deg_s = gx * (250.0/32768.0);
+  float gy_deg_s = gy * (250.0/32768.0);
+  float gz_deg_s = gz * (250.0/32768.0);
+  
+  
+  dt = (millis()-tiempo_prev)/1000.0;
+  tiempo_prev=millis();
+  
+  //Calcular los ángulos con acelerometro
+  float accel_ang_x=atan(ay_m_s2/sqrt(pow(ax_m_s2,2) + pow(az_m_s2,2)))*(180.0/3.14);
+  float accel_ang_y=atan(-ax_m_s2/sqrt(pow(ay_m_s2,2) + pow(az_m_s2,2)))*(180.0/3.14);
+  
+  //Calcular angulo de rotación con giroscopio y filtro complemento  
+  ang_x = 0.98*(ang_x_prev+(gx_deg_s/131)*dt) + 0.02*accel_ang_x;
+  ang_y = 0.98*(ang_y_prev+(gy_deg_s/131)*dt) + 0.02*accel_ang_y;
+  
+  
+  
+  ang_x_prev=ang_x;
+  ang_y_prev=ang_y;
+
+  //Mostrar los angulos separadas por un [tab]
+
+  Serial.print("Rotacion en X:  ");
+  Serial.print(ang_x); 
+  Serial.print("tRotacion en Y: ");
+  Serial.println(ang_y);
+
+  delay(10);
+}
